2e39dd4eddb60e0d44ce4ceb6cd92e6e42da11e2
evarodriguezdelgado <erodriguezdelgado@al.uloyola.es>
2026-03-03 10:20:16 +0100

    automatizacion de creacion de roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        // Llamamos al seeder de roles para automatizar la creación de perfiles admin, tutor e intern
+        $this->call([RoleSeeder::class]);
 
         User::factory()->create([
             'name' => 'Test User',
