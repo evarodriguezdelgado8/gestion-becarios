901a7b21fe37ee778edbb011d79bb330ecfd6e39
evarodriguezdelgado <erodriguezdelgado@al.uloyola.es>
2026-05-27 15:04:45 +0200

    fix: forzar Https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 use Illuminate\Support\Facades\Gate;
@@ -26,9 +27,13 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->configureDefaults();
 
-        Gate::before(function ($user, $ability) {
+                Gate::before(function ($user, $ability) {
             return $user->hasRole('admin') ? true : null;
         });
+
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 
     /**
